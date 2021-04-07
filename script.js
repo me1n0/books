7b0289c9e15d5df9ba9609909117c1e2f6c7411a
Danil <80169113+drngry@users.noreply.github.com>
2021-04-07 20:06:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+new Swiper(".swiper-container", {
+    speed: 400,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+  
